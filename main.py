1cb5f50a73ed8937471130579b9cb08e6b375466
Matan Budimir <matanbudimir@gmail.com>
2020-09-26 14:19:07 +0200

    Add some comments

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 import csv
 import argparse
 
+"""
+    CSV Columns
+"""
 DEP=3
 ARR=4
 ROUTE=5
@@ -11,12 +14,23 @@ REMARKS=6
 URL="https://grd.aero-nav.com/"
 CALLSIGN=7
 
+"""
+    format_url returns formated GRD URL
+"""
+
 def format_url(dep, arr):
     return f"{URL}?From={dep}&To={arr}"
 
+"""
+    Create file argument
+"""
 parser = argparse.ArgumentParser()
 parser.add_argument("-file", "--file", type=str, required=False, default=None)
 args = parser.parse_args()
+
+"""
+    Check is file argument empty. If it is show the error and exit
+"""
 
 if args.file == None:
     print("File not specified.")
